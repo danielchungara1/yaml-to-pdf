4eb2a3177a82ba8b4b8ee3f616da944db92ba558
daniel chungara <danielchungaraespinlabs@gmail.com>
2020-03-17 00:04:36 +1200

    projecto inicial

---
+++ src/main/java/ar/com/example/App.java
@@ -0,0 +1,14 @@
+package ar.com.example;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+//        APLICACION CONSOLA JAVA - CONVERSION YAML-TO-PDF
+//            -Recibe el PATH del archivo yaml a convertir
+//            -Convertir el YAML-TO-PDF
+//            -Guardar el archivo PDF en el mismo directorio del archivo YAML-TO-PDF
+//            -Mostrar mensaje de exito.
+
+    }
+}
